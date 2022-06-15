1174ef26aa2d866d7337112bf10f1075117f9fcb
Daniel Higgins <daniel.higgins@gmail.com>
2022-06-15 09:00:22 -0400

    spinner overlay

--- src/FullPageSpinner.tsx
+++ src/FullPageSpinner.tsx
@@ -1,15 +1,18 @@
+import ReactDOM from "react-dom";
+
 export default function FullPageSpinner() {
-  return (
-    <div className="overlay">
+  return ReactDOM.createPortal(
+    <div className="overlay" style={{ zIndex: 2000 }}>
       <div className="d-flex justify-content-center">
         <div
           className="spinner-grow text-primary"
           role="status"
-          style={{ width: "3rem", height: "3rem", zIndex: 10000 }}
+          style={{ width: "3rem", height: "3rem" }}
         >
           <span className="sr-only">Loading...</span>
         </div>
       </div>
-    </div>
+    </div>,
+    document.getElementsByTagName("body")[0]
   );
 }
