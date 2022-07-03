10515edd507483bf164ffce37862754b65f92c52
Daniel Higgins <daniel.higgins@gmail.com>
2022-07-03 10:56:49 -0400

    update lock file

--- src/ShoppingList.tsx
+++ src/ShoppingList.tsx
@@ -1,3 +1,16 @@
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Input } from "reactstrap";
+
 export default function ShoppingList() {
-  return <div>shopping list</div>;
+  return (
+    <div>
+      <div className="input-group">
+        <Input placeholder="Add item..." />
+        <Button color="secondary">
+          <FontAwesomeIcon icon={faCheck} />
+        </Button>
+      </div>
+    </div>
+  );
 }
