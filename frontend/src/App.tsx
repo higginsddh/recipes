94208624ba143e2af2ed6e47c9b8f9b94cad23a9
Daniel Higgins <daniel.higgins@gmail.com>
2022-10-30 15:06:46 -0400

    offline test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,14 +6,39 @@ import { Routes, Route } from "react-router-dom";
 import ShoppingList from "./shoppingList/ShoppingList";
 import { Toaster } from "react-hot-toast";
 import RealTimeWrapper from "./RealTimeWrapper";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import {
+  persistQueryClient,
+  PersistQueryClientProvider,
+} from "@tanstack/react-query-persist-client";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      networkMode: "offlineFirst",
+      cacheTime: 1000 * 60 * 60 * 24 * 365, // 1 year
+    },
+  },
+});
+
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+// const sessionStoragePersister = createSyncStoragePersister({ storage: window.sessionStorage })
+
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
+});
 
 export default function App() {
   return (
     <>
       <Toaster />
-      <QueryClientProvider client={queryClient}>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister: localStoragePersister }}
+      >
         <RealTimeWrapper>
           <AppNavbar />
           <div className="container mt-4">
@@ -23,7 +48,7 @@ export default function App() {
             </Routes>
           </div>
         </RealTimeWrapper>
-      </QueryClientProvider>
+      </PersistQueryClientProvider>
     </>
   );
 }
